2d264290b86b8279e4bfc46e806d91a7b2ee3050
Letto <rodrigo0830@gmail.com>
2022-04-06 20:21:05 -0500

    correcion PORT

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from './app'
 import './database'
 import { PORT } from './config'
 
-app.listen(3000)
-console.log('El protocolo K.F.S. se está ejecutando en el puerto', 3000)
+app.listen(PORT)
+console.log('El protocolo K.F.S. se está ejecutando en el puerto', PORT)
